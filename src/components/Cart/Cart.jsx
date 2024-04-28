e1e376d07b925c3fb01d5e477a7b0f8ec595786c
Salah AlDin <salahqerem@gmail.com>
2024-04-29 01:44:36 +0300

    Add total amount calcultion for the cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Drawer, List } from "@mui/material";
+import { Box, Divider, Drawer, List, Typography } from "@mui/material";
 import React from "react";
 import CartItem from "./components/CartItem.jsx";
 import { CustomSuccessAlert } from "../utils/functions.js";
@@ -11,6 +11,12 @@ const Cart = ({ cartProducts, dispatchCart, toggleCartDrawer, cartOpen }) => {
     });
     CustomSuccessAlert("This Product has been deleted");
   };
+
+  const totalPrice = cartProducts.reduce(
+    (total, product) => total + product.price,
+    0
+  );
+
   const renderCartProducts = cartProducts.map((product) => (
     <CartItem
       key={product.id}
@@ -20,9 +26,25 @@ const Cart = ({ cartProducts, dispatchCart, toggleCartDrawer, cartOpen }) => {
   ));
   return (
     <Drawer anchor="right" open={cartOpen} onClose={toggleCartDrawer(false)}>
-      <Box sx={{ width: 500 }} role="presentation">
+      <Box sx={{ width: 520 }} role="presentation">
         <List>{renderCartProducts}</List>
-        <Divider />
+        <Box
+          sx={{
+            position: "fixed",
+            bottom: 0,
+            right: 0,
+            width: 520,
+            backgroundColor: "white",
+            padding: "16px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            borderBlockStart: "1px solid",
+          }}
+        >
+          <Typography variant="h6">Total Price:</Typography>
+          <Typography variant="h6">${totalPrice}</Typography>
+        </Box>
       </Box>
     </Drawer>
   );
